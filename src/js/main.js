518089159a9af18387ca66537a192974ab21d868
Vlad Basoon <vbasoon@gmail.com>
2021-10-04 14:42:09 +0300

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,18 @@ let img = document.querySelectorAll(".home__slider img").forEach((images) => {
     document.querySelector(".home__image img").src = src;
   };
 });
+
+const swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true;
+});
